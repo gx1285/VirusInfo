b6a31adfd32c70392f6cd76c7e13cab5f00fb885
gx1285 <78240988+gx1285@users.noreply.github.com>
2022-03-09 16:02:00 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,16 @@
 from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
-with open('requirements.txt') as f:
-  requirements = f.read().splitlines()
 setup(
     name='VirusInfo',
-    version='0.2.0',
+    version='0.2.1',
     description='Library for obtaining the number of people infected with COVID-19',
     long_description=long_description,
     long_description_content_type='text/markdown',
     author='gx1285',
     author_email="runay2342@gmail.com",
     url="https://github.com/gx1285/VirusInfo",
-    install_requires=requirements,
+    install_requires=requests,
     python_requires='>=3.8.0',
     license='MIT',
     classifiers=[
