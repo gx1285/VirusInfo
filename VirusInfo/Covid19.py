40e1e6f3d2c63a9170fc5633c76e5b395f34a57e
gx1285 <78240988+gx1285@users.noreply.github.com>
2022-03-09 16:17:55 +0900

    Update Covid19.py

--- VirusInfo/Covid19.py
+++ VirusInfo/Covid19.py
@@ -5,4 +5,6 @@ def covid_19_ja(date=None):
   req =  requests.get(f"https://opendata.corona.go.jp/api/Covid19JapanNdeaths?date={date}")
   text = req.text.replace('-', '')
   s = text.replace('{', '').replace('"', '').replace('errorInfo:errorFlag:0,errorCode:null,errorMessage:null},itemList:[date', '').replace(f'{date}', '').replace(':,ndeaths:', '').replace('}]}', '')
+  if s == "errorInfo:errorFlag:0,errorCode:null,errorMessage:null},itemList:[]}":
+    raise TypeError('Data for this date could not be found.')
   return s
