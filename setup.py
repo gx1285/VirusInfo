cb12e5583ed09d4fd83c85fdf85c5f9250f66607
gx1285 <78240988+gx1285@users.noreply.github.com>
2022-03-09 15:48:41 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
   requirements = f.read().splitlines()
 setup(
     name='VirusInfo',
-    version='0.1.0',
+    version='0.2.0',
     description='Library for obtaining the number of people infected with COVID-19',
     long_description=long_description,
     long_description_content_type='text/markdown',
