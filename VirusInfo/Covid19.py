829a60a11db51483261a0a48eab5401703040227
gx1285 <78240988+gx1285@users.noreply.github.com>
2022-03-09 17:33:29 +0900

    Update Covid19.py

--- VirusInfo/Covid19.py
+++ VirusInfo/Covid19.py
@@ -1,10 +1,11 @@
 import requests
-def covid_19_ja(date=None):
-  if date == None:
-    raise TypeError('Argument "date" is missing.')
-  req =  requests.get(f"https://opendata.corona.go.jp/api/Covid19JapanNdeaths?date={date}")
-  text = req.text.replace('-', '')
-  s = text.replace('{', '').replace('"', '').replace('errorInfo:errorFlag:0,errorCode:null,errorMessage:null},itemList:[date', '').replace(f'{date}', '').replace(':,ndeaths:', '').replace('}]}', '')
-  if s == "errorInfo:errorFlag:0,errorCode:null,errorMessage:null},itemList:[]}":
-    raise TypeError('Data for this date could not be found.')
-  return s
+class Covid_19:
+  def japan(date=None):
+    if date == None:
+      raise TypeError('Argument "date" is missing.')
+    req =  requests.get(f"https://opendata.corona.go.jp/api/Covid19JapanNdeaths?date={date}")
+    text = req.text.replace('-', '')
+    s = text.replace('{', '').replace('"', '').replace('errorInfo:errorFlag:0,errorCode:null,errorMessage:null},itemList:[date', '').replace(f'{date}', '').replace(':,ndeaths:', '').replace('}]}', '')
+    if s == "errorInfo:errorFlag:0,errorCode:null,errorMessage:null},itemList:[]}":
+      raise TypeError('Data for this date could not be found.')
+    return s
