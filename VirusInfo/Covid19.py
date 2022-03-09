047adfe37b5f41e6cadaa920842f1ba2568babfc
gx1285 <78240988+gx1285@users.noreply.github.com>
2022-03-09 19:08:22 +0900

    Update Covid19.py

--- VirusInfo/Covid19.py
+++ VirusInfo/Covid19.py
@@ -8,4 +8,6 @@ class Covid_19:
     s = text.replace('{', '').replace('"', '').replace('errorInfo:errorFlag:0,errorCode:null,errorMessage:null},itemList:[date', '').replace(f'{date}', '').replace(':,ndeaths:', '').replace('}]}', '')
     if s == "errorInfo:errorFlag:0,errorCode:null,errorMessage:null},itemList:[]}":
       raise TypeError('Data for this date could not be found.')
+    elif s == "errorInfo:errorFlag:1,errorCode:100,errorMessage:日付の形式が不正です。},itemList:[]}":
+      raise TypeError('Date format is invalid.')
     return s
