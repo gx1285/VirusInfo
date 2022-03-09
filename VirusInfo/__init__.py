c0c95203fb70753dfba9ca5ac9319c19cd762061
gx1285 <78240988+gx1285@users.noreply.github.com>
2022-03-09 12:12:04 +0900

    Create __init__.py

---
+++ VirusInfo/__init__.py
@@ -0,0 +1,8 @@
+"""
+Virus Info
+~~~~~~~~~~~~~~~~~~~
+:copyright: (c) 2022-present gx1285
+:license: MIT, see LICENSE for more details.
+"""
+from VirusInfo.VirusInfo import *
+from VirusInfo.Covid19 import *
