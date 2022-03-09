4809d093e6250c1793bf3e7025f0415e8d0eb1aa
gx1285 <78240988+gx1285@users.noreply.github.com>
2022-03-09 15:42:47 +0900

    Update Covid19.py

--- VirusInfo/Covid19.py
+++ VirusInfo/Covid19.py
@@ -1,1 +1,8 @@
-
+import requests
+def covid_19_ja(date=None):
+  if date == None:
+    raise TypeError('Argument "date" is missing.')
+  req =  requests.get(f"https://opendata.corona.go.jp/api/Covid19JapanNdeaths?date={date}")
+  text = req.text.replace('-', '')
+  s = text.replace('{', '').replace('"', '').replace('errorInfo:errorFlag:0,errorCode:null,errorMessage:null},itemList:[date', '').replace(f'{date}', '').replace(':,ndeaths:', '').replace('}]}', '')
+  return s
