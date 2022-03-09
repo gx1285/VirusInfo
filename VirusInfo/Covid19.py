64936e05057ab7a8f248cc4692ae33a2d0c686ed
gx1285 <78240988+gx1285@users.noreply.github.com>
2022-03-09 19:50:23 +0900

    Update Covid19.py

--- VirusInfo/Covid19.py
+++ VirusInfo/Covid19.py
@@ -1,9 +1,17 @@
 import requests
 class Covid_19:
+  """
+  Information on the number of people infected with Covid-19 and other information can be obtained.
+  Added: v0.4
+  """
   def japan(date=None):
+    """
+    Number of infected persons on selected dates in Japan.
+    Added: v0.2
+    """    
     if date == None:
       raise TypeError('Argument "date" is missing.')
-    req =  requests.get(f"https://opendata.corona.go.jp/api/Covid19JapanNdeaths?date={date}")
+    req = requests.get(f"https://opendata.corona.go.jp/api/Covid19JapanNdeaths?date={date}")
     text = req.text.replace('-', '')
     s = text.replace('{', '').replace('"', '').replace('errorInfo:errorFlag:0,errorCode:null,errorMessage:null},itemList:[date', '').replace(f'{date}', '').replace(':,ndeaths:', '').replace('}]}', '')
     if s == "errorInfo:errorFlag:0,errorCode:null,errorMessage:null},itemList:[]}":
@@ -11,3 +19,15 @@ class Covid_19:
     elif s == "errorInfo:errorFlag:1,errorCode:100,errorMessage:日付の形式が不正です。},itemList:[]}":
       raise TypeError('Date format is invalid.')
     return s
+  def castam(city=None, date=None):
+    """Cumulative number of infected people in the specified area up to the specified date
+    Added: v0.4
+    """
+    if city == None:
+      raise TypeError('Argument "city" is missing.')
+    elif date == None:
+      raise TypeError('Argument "date" is missing.')
+    req1 = requests.get(f"https://opendata.corona.go.jp/api/Covid19JapanAll?date={date}&dataName={city}")
+    text1 = req1.text.replace('-', '')
+    s2 = text1.replace('{"errorInfo":{"errorFlag":"0","errorCode":null,"errorMessage":null},"itemList":[{"date":"', '').replace('","name_jp":"', '').replace(f"{city}", '').replace(f"{date}", '').replace('","npatients":"', '').replace('"}]}', '')
+    return s2
