0e0b880349c7074ba9d30557481507b6dd3ff5b6
gx1285 <78240988+gx1285@users.noreply.github.com>
2022-03-11 15:50:38 +0900

    Additional features in 0.4.0.

--- VirusInfo/Covid19.py
+++ VirusInfo/Covid19.py
@@ -19,21 +19,18 @@ class Covid_19:
     elif s2 == "errorInfo:errorFlag:1,errorCode:100,errorMessage:日付の形式が不正です。},itemList:[]}":
       raise TypeError('Date format is invalid.')
     return s2
-  def japan(date=None):
+
+  def japan_all():
     """
     Number of infected persons on selected dates in Japan.
-    Added: v0.2
+    Added: v0.4
     """    
-    if date == None:
-      raise TypeError('Argument "date" is missing.')
-    req = requests.get(f"https://opendata.corona.go.jp/api/Covid19JapanAll?date={date}")
-    text = req.text.replace('-', '')
-    s = text.replace('{', '').replace('"', '').replace('errorInfo:errorFlag:0,errorCode:null,errorMessage:null},itemList:[date', '').replace(f'{date}', '').replace(':,ndeaths:', '').replace('}]}', '')
-    if s == "errorInfo:errorFlag:0,errorCode:null,errorMessage:null},itemList:[]}":
-      raise TypeError('Data for this date could not be found.')
-    elif s == "errorInfo:errorFlag:1,errorCode:100,errorMessage:日付の形式が不正です。},itemList:[]}":
-      raise TypeError('Date format is invalid.')
-    return s
+    req3 = requests.get(f"https://data.corona.go.jp/converted-json/covid19japan-all.json")
+    text3 = req3.text.replace(f'","','\n')
+    s3 = text3.replace('[{"srcurl_pdf": "", "srcurl_web": "", "description": "", "npatients": 0, "nexits": 0, "ndeaths": 0, "ncurrentpatients": 0, "srcurl_pdf_archived": "", "lastUpdate": "','').replace('", "area": [{"','\n').replace('", "name": "", "ncurrentpatients": 0, "nexits": 0, "ndeaths": 0, "npatients": ','').replace('name_jp": "','').replace('},','\n').replace('{"','').replace('}]}]','')
+    return s3
+
+
   def castam(city=None, date=None):
     """Cumulative number of infected people in the specified area up to the specified date
     Added: v0.4
